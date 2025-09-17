8ca27d8272e13631f71885c12c027ff4207ff908
Faqih <80795963+faqih28alam@users.noreply.github.com>
2025-09-17 12:58:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if page == "Home":
     st.subheader("My Models")
     # List of image URLs (replace/add your own)
     images = [
-        "Streamlit/heart-disease.jpg",
+        "heart-disease.jpg",
         "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
         "https://images.unsplash.com/photo-1465101046530-73398c7f28ca",
         "https://images.unsplash.com/photo-1454023492550-5696f8ff10e1",
@@ -181,6 +181,7 @@ elif page == "Contact":
 )
 
 # ...existing model code...
+
 
 
 
